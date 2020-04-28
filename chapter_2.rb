6ae3e979f14af0306870269d2c3ea3d78d710d44
atz4_s <47665248+atz4-s@users.noreply.github.com>
2020-04-28 10:33:15 +0900

    chapter2の解答

---
+++ chapter_2.rb
@@ -0,0 +1,28 @@
+#問1
+puts 2 + 3
+#問2
+puts 2 * 2 * 3.14
+#問3
+puts "Ruby"
+#問4
+puts "abc" +  "def"
+#問5
+puts "123".to_i + "456".to_i
+#問6
+coffee = 300
+espresso = 100
+puts "コーヒー:#{coffee}円"
+puts "合計：#{coffee + espresso * 2}円"
+#問7
+coffee = 400
+espresso = 100
+puts "コーヒー:#{coffee}円"
+puts "合計：#{coffee + espresso * 2}円"
+#問8
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
+#問9
+total = 300 * 2
+puts total
